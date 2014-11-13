9e31c65d16fd4c3c1f10578397ce1025a4e5d076
Bui Thanh Hai <hai.bth@gmail.com>
2014-11-13 12:00:28 +0700

    Add SimpleForm and fix the first step to select options in New Product form

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,7 +1,8 @@
 class Option < ActiveRecord::Base
+
   has_many :variants
-  has_many :products, through: :product_options
   has_many :prduct_options
+  has_many :products, through: :product_options
 
   accepts_nested_attributes_for :variants, allow_destroy: true
 
--- app/models/product_option.rb
+++ app/models/product_option.rb
@@ -2,6 +2,15 @@ class ProductOption < ActiveRecord::Base
   belongs_to :product
   belongs_to :option
 
-  accepts_nested_attributes_for :option
+  has_many :variants, through: :option
+  accepts_nested_attributes_for :variants
+
+  #def variants
+  #	return option.variants
+  #end
+
+  #def variants=(params)
+  #	Rails.logger.info "Option Variants params: #{params}"
+  #end
 
 end
